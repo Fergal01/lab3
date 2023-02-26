59e6663d5a21d77bcaf29fb656b0efd49b62bf71
Fergal01 <125209132+Fergal01@users.noreply.github.com>
2023-02-26 23:41:45 +0700

    Add files via upload

---
+++ Fraction.java
@@ -0,0 +1,18 @@
+package lab3;
+
+public class Fraction {
+        public int namerator;
+        public int denaminator;
+        public Fraction(){
+            this.namerator=0;
+            this.denaminator=0;
+        }
+        public Fraction(int num, int den){
+            this.namerator=num;
+            this.denaminator=den;
+        }
+        public Fraction(f Fraction){
+            this.denaminator=Fraction;
+            this.namerator=Faction;
+        }
+}
